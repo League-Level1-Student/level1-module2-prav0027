9a47432969bdb12c19b4c8442cf2b793bf748470
league <league@wts8.attlocal.net>
2018-11-29 19:53:10 -0800

    Intro to Getters and Setters (IP)

---
+++ src/Taco.java
@@ -0,0 +1,24 @@
+
+public class Taco {
+public static void main(String[] args) {	
+
+}
+private String meat() {
+	return "meat";
+}
+private String sauce() {
+	return "sauce";
+}
+private void getMeat() {
+	meat();
+}
+private void getSauce() {
+	sauce();
+}
+private void setMeat(String meat) {
+	
+}
+private void setSauce(String sauce) {
+	
+}
+}
